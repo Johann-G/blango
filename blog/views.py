577b72a120668a54433758b316aa109f21033243
johann groll <groll.johann@gmail.com>
2024-06-05 12:21:09 +0200

    init logging

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    logger.debug("%d posts available", len(posts))
+    return render(request, "post_list.html", {"posts": posts})
+
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, "post_detail.html", {"post": post})
